0c6bf6d61c7f449085cee0a5d0d0f6b4e89c40ec
Joel Smith <Hikash@gmail.com>
2020-07-23 12:07:32 -0700

    lock the map to prevent concurrent writes

--- backend/contentstore/memstore.go
+++ backend/contentstore/memstore.go
@@ -73,6 +73,8 @@ func (d *MemStore) Delete(key string) error {
 	if _, ok := d.content[key]; !ok { // artificial behavior to match other stores
 		return backend.WrapError("Unable to delete from MemStore", fmt.Errorf("No such key"))
 	}
+	d.mutex.Lock()
 	delete(d.content, key)
+	d.mutex.Unlock()
 	return nil
 }
