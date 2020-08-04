6a09f49e5a84702669f30fac9d779ff2d87a35f3
Joel Smith <joel.smith@originate.com>
2020-08-04 13:38:06 -0700

    TagTable usability improvements (#36)

--- frontend/src/components/table/index.tsx
+++ frontend/src/components/table/index.tsx
@@ -5,23 +5,48 @@ import * as React from 'react'
 import classnames from 'classnames/bind'
 const cx = classnames.bind(require('./stylesheet'))
 
+export const SortAsc = 'asc'
+export const SortDesc = 'desc'
+
+export type SortDirection = typeof SortAsc | typeof SortDesc | undefined
+
+export type ColumnData = {
+  label: string,
+  title: string,
+  sortDirection?: SortDirection,
+  clickable?: boolean
+}
+
 export default (props: {
   children: React.ReactNode,
   className?: string,
-  columns: Array<string | {label: string, title: string}>,
+  columns: Array<string | ColumnData>,
+  onColumnClicked?: (colIndex: number)=>void,
 }) => (
-  <table className={cx('root', props.className)}>
-    <thead>
-      <tr>
-        {props.columns.map((column, idx) => (
-          typeof (column) === 'object'
-            ? <th key={`${column.label}-${idx}`} title={column.title}>{column.label}</th>
-            : <th key={`${column}-${idx}`}>{column}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {props.children}
-    </tbody>
-  </table>
-)
+    <table className={cx('root', props.className)}>
+      <thead>
+        <tr>
+          {props.columns.map((column, idx) => {
+            if (typeof (column) === 'object') {
+              const args = {
+                key: `${column.label}-${idx}`,
+                title: column.title,
+                style: column.clickable ? { cursor: "pointer" } : {},
+                onClick: () => (column.clickable && props.onColumnClicked) ? props.onColumnClicked(idx) : undefined
+              }
+              return (
+                <th {...args}>
+                  {column.label}
+                  {column.sortDirection ? <span className={cx('arrow', column.sortDirection == SortAsc ? 'asc' : 'desc')}></span> : null}
+                </th>
+              )
+            }
+            return <th key={`${column}-${idx}`}>{column}</th>
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {props.children}
+      </tbody>
+    </table>
+  )
