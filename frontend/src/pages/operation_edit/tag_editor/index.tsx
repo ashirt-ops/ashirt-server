ec187e5df236a362b73305a9ba7e054d4e5b37ab
Joel Smith <Hikash@gmail.com>
2020-08-04 08:28:14 -0700

    add table sorting support

--- frontend/src/pages/operation_edit/tag_editor/index.tsx
+++ frontend/src/pages/operation_edit/tag_editor/index.tsx
@@ -3,16 +3,22 @@
 
 import * as React from 'react'
 import SettingsSection from 'src/components/settings_section'
-import Table from 'src/components/table'
+import { default as Table, SortAsc, SortDesc, SortDirection, ColumnData } from 'src/components/table'
 import Tag from 'src/components/tag'
-import {DeleteTagModal, EditTagModal} from './modals'
+import { DeleteTagModal, EditTagModal } from './modals'
 import { TagWithUsage } from 'src/global_types'
-import {default as Button, ButtonGroup} from 'src/components/button'
-import {getTags} from 'src/services'
-import {useWiredData, useModal, renderModals} from 'src/helpers'
+import { default as Button, ButtonGroup } from 'src/components/button'
+import { getTags } from 'src/services'
+import { useWiredData, useModal, renderModals } from 'src/helpers'
 
 // @ts-ignore - npm package @types/react-router-dom needs to be updated (https://github.com/DefinitelyTyped/DefinitelyTyped/issues/40131)
 import { useHistory } from 'react-router-dom'
+
+type compareableFunc = (l: any, r: any) => number
+
+const sortNone: compareableFunc = (a: any, b: any) => 0
+const sortNums: compareableFunc = (a: TagWithUsage, b: TagWithUsage) => a.evidenceCount - b.evidenceCount
+const sortTags: compareableFunc = (a: TagWithUsage, b: TagWithUsage) => a.name.localeCompare(b.name)
 
 const TagTable = (props: {
   operationSlug: string,
@@ -27,16 +33,37 @@ const TagTable = (props: {
     <DeleteTagModal {...modalProps} operationSlug={props.operationSlug} onDeleted={props.onUpdate} />
   ))
 
+  // React has a special meaning for passing functions into useState, so re-wrapping the function to
+  // simply store the desired sorting function (we do the same with the setSortFunc value)
+  const [sortFunc, setSortFunc] = React.useState<compareableFunc>(() => sortNone)
+
+  const [columns, setColumns] = React.useState<Array<ColumnData & { compareVia: compareableFunc }>>([
+    { title: '', label: 'Tag', clickable: true, compareVia: sortTags },
+    { title: '', label: '# Evidence Attached To', clickable: true, compareVia: sortNums },
+    { title: '', label: 'Actions', compareVia: sortNone },
+  ])
+
+  const updateColumnSorting = (index: number) => {
+    const sortDirections: Array<{ compare: compareableFunc, dir: SortDirection }> = [
+      { dir: SortAsc, compare: columns[index].compareVia },
+      { dir: SortDesc, compare: (a, b) => columns[index].compareVia(b, a) },
+      { dir: undefined, compare: sortNone }
+    ]
+    const matchIndex = (sortDirections.findIndex(x => x.dir == columns[index].sortDirection) + 1) % sortDirections.length
+    setColumns(columns.map((col, idx) => ({ ...col, sortDirection: (idx == index) ? sortDirections[matchIndex].dir : undefined })))
+    setSortFunc(() => sortDirections[matchIndex].compare)
+  }
+
   return <>
-    <Table columns={['Tag', '# Evidence Attached To', 'Actions']}>
-      {props.tags.map(tag => (
+    <Table columns={columns} onColumnClicked={updateColumnSorting}>
+      {[...props.tags].sort(sortFunc).map(tag => (
         <tr key={tag.name}>
-          <td><Tag name={tag.name} color={tag.colorName} onClick={() => history.push(`/operations/${props.operationSlug}/evidence?q=tag:${tag.name}`)}/></td>
+          <td><Tag name={tag.name} color={tag.colorName} onClick={() => history.push(`/operations/${props.operationSlug}/evidence?q=tag:${tag.name}`)} /></td>
           <td>{tag.evidenceCount}</td>
           <td>
             <ButtonGroup>
-              <Button small onClick={() => editTagModal.show({tag})}>Edit</Button>
-              <Button small onClick={() => deleteTagModal.show({tag})}>Delete</Button>
+              <Button small onClick={() => editTagModal.show({ tag })}>Edit</Button>
+              <Button small onClick={() => deleteTagModal.show({ tag })}>Delete</Button>
             </ButtonGroup>
           </td>
         </tr>
@@ -50,7 +77,7 @@ const TagTable = (props: {
 export default (props: {
   operationSlug: string,
 }) => {
-  const wiredTags = useWiredData(React.useCallback(() => getTags({operationSlug: props.operationSlug}), [props.operationSlug]))
+  const wiredTags = useWiredData(React.useCallback(() => getTags({ operationSlug: props.operationSlug }), [props.operationSlug]))
 
   return (
     <SettingsSection title="Operation Tags">
