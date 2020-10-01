85aea9c89ce8034d47acf8d30c98e66f2634e25d
Joel Smith <Hikash@gmail.com>
2020-10-01 12:01:51 -0700

    add testing for local totp

---
+++ backend/authschemes/localauth/services_internal_test.go
@@ -0,0 +1,122 @@
+package localauth
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/theparanoids/ashirt-server/backend/authschemes"
+	"github.com/theparanoids/ashirt-server/backend/database/seeding"
+	"github.com/theparanoids/ashirt-server/backend/helpers"
+	"github.com/theparanoids/ashirt-server/backend/session"
+)
+
+func initBridge(t *testing.T) authschemes.AShirtAuthBridge {
+	db := seeding.InitTestWithOptions(t, seeding.TestOptions{
+		DatabasePath: helpers.StringPtr("../../migrations"),
+		DatabaseName: helpers.StringPtr("auth-test-db"),
+	})
+	seeding.ApplySeeding(t, seeding.HarryPotterSeedData, db)
+	sessionStore, err := session.NewStore(db, session.StoreOptions{SessionDuration: time.Hour, Key: []byte{}})
+	require.NoError(t, err)
+	return authschemes.MakeAuthBridge(db, sessionStore, "local")
+}
+
+func TestReadUserTotpStatus(t *testing.T) {
+	bridge := initBridge(t)
+
+	deviousUser := seeding.UserDraco
+	adminUser := seeding.UserDumbledore
+	targetUser := seeding.UserRon
+
+	var ctx context.Context
+	var hasTotp bool
+	var err error
+
+	// verify target user ("self") does not have totp
+	ctx = seeding.SimpleFullContext(targetUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, targetUser.Slug) // with slug
+	require.NoError(t, err)
+	require.False(t, hasTotp)
+
+	ctx = seeding.SimpleFullContext(targetUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, "") // without slug
+	require.NoError(t, err)
+	require.False(t, hasTotp)
+
+	// verify that devious user cannot check totp status for target user
+	ctx = seeding.SimpleFullContext(deviousUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, targetUser.Slug)
+	require.Error(t, err)
+
+	// verify admin _can_ check totp status for target user
+	ctx = seeding.SimpleFullContext(adminUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, targetUser.Slug)
+	require.NoError(t, err)
+	require.False(t, hasTotp)
+
+	// give target user totp -- note: this will remove existing encrypted_password and must_reset_password values
+	err = bridge.UpdateAuthForUser(authschemes.UserAuthData{
+		TOTPSecret: helpers.StringPtr("abc123"),
+		UserKey:    targetUser.FirstName,
+	})
+	require.NoError(t, err)
+
+	// verify that target now has totp
+	ctx = seeding.SimpleFullContext(targetUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, "")
+	require.NoError(t, err)
+	require.True(t, hasTotp)
+
+	// verify admin sees totp change status for target user
+	ctx = seeding.SimpleFullContext(adminUser)
+	hasTotp, err = readUserTotpStatus(ctx, bridge, targetUser.Slug)
+	require.NoError(t, err)
+	require.True(t, hasTotp)
+}
+
+func TestDeleteUserTotp(t *testing.T) {
+	bridge := initBridge(t)
+
+	deviousUser := seeding.UserDraco
+	adminUser := seeding.UserDumbledore
+	targetUser := seeding.UserRon
+
+	var ctx context.Context
+	var err error
+
+	// give target user TOTP
+	err = bridge.UpdateAuthForUser(authschemes.UserAuthData{
+		TOTPSecret: helpers.StringPtr("abc123"),
+		UserKey:    targetUser.FirstName,
+	})
+	require.NoError(t, err)
+
+	// verify that a devious user cannot remove totp for a target user
+	ctx = seeding.SimpleFullContext(deviousUser)
+	err = deleteUserTotp(ctx, bridge, targetUser.Slug)
+	require.Error(t, err)
+
+	// verify target user can remove their own totp
+	ctx = seeding.SimpleFullContext(targetUser)
+	err = deleteUserTotp(ctx, bridge, "")
+	require.NoError(t, err)
+
+	// verify admin cannot "remove" target user totp (for user that does not have totp)
+	ctx = seeding.SimpleFullContext(adminUser)
+	err = deleteUserTotp(ctx, bridge, targetUser.Slug)
+	require.Error(t, err)
+
+	// re-give user totp
+	err = bridge.UpdateAuthForUser(authschemes.UserAuthData{
+		TOTPSecret: helpers.StringPtr("abc123"),
+		UserKey:    targetUser.FirstName,
+	})
+	require.NoError(t, err)
+
+	// verify admin can remove target user totp
+	ctx = seeding.SimpleFullContext(adminUser)
+	err = deleteUserTotp(ctx, bridge, targetUser.Slug)
+	require.NoError(t, err)
+}
