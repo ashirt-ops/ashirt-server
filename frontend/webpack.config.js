839222dbf7a572602cd44506627fa3921976935d
Joel Smith <Hikash@gmail.com>
2020-10-12 15:08:28 -0700

    serve assets from /assets

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -77,6 +77,7 @@ module.exports = (env, argv) => ({
 
   devServer: {
     historyApiFallback: true,
+    publicPath: "/assets/",
     proxy: {
       '/web': {target: process.env.WEB_BACKEND_ORIGIN},
     },
