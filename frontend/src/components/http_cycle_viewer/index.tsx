0b6df540f99979c608c1b1864a2db63e0ac562e7
Joel Smith <joel.smith@originate.com>
2021-01-12 15:55:34 -0800

    refactor into smaller components,minor style fixes

--- frontend/src/components/http_cycle_viewer/index.tsx
+++ frontend/src/components/http_cycle_viewer/index.tsx
@@ -19,93 +19,113 @@ export default (props: {
   log: Har
 }) => {
   const log: Log = props.log.log
-
   const [selectedRow, setSelectedRow] = React.useState<number>(-1)
 
+  return (
+    <div className={cx('root')} onClick={e => e.stopPropagation()}>
+      <EvidenceHeader creator={log.creator.name} version={log.creator.version}/>
+      <RequestTable log={log} selectedRow={selectedRow} setSelectedRow={setSelectedRow}/>
+      {selectedRow > -1 && <HttpDetails entry={log.entries[selectedRow]} />}
+    </div>
+  )
+}
+
+const EvidenceHeader = (props:{
+  creator: string,
+  version: string
+}) => (
+  <div className={cx('header')}>
+    From:
+    <em className={cx('header-creator')}>
+      {props.creator}
+    </em>
+    @
+    <em className={cx('header-version')}>
+      {props.version}
+    </em>
+  </div>
+)
+
+const RequestTable = (props: {
+  log: Log
+  selectedRow: number
+  setSelectedRow: (rowNumber: number) => void
+}) => {
   const tbodyRef = React.useRef<HTMLTableSectionElement | null>(null)
   const onKeyDown = (e: KeyboardEvent) => {
     if (['ArrowUp', 'ArrowDown'].includes(e.key)) {
       e.stopPropagation()
       e.preventDefault()
 
-      const newIndex = clamp(selectedRow + (e.key == 'ArrowDown' ? 1 : -1), 0, log.entries.length - 1)
+      const newIndex = clamp(props.selectedRow + (e.key == 'ArrowDown' ? 1 : -1), 0, props.log.entries.length - 1)
       if (tbodyRef.current != null) {
         // @ts-ignore - typescript is unable to determine that children is an array of HTMLDivElements
         const rows: Array<HTMLTableRowElement> = Array.from(tbodyRef.current.children).filter(el => el instanceof HTMLTableRowElement)
         rows[newIndex].scrollIntoView() // this needs adjustment to be less weird/active
       }
-      setSelectedRow(newIndex)
+      props.setSelectedRow(newIndex)
     }
   }
 
   return (
-    <div className={cx('root')} onClick={e => e.stopPropagation()} >
-      <div className={cx('header')}>From: <em>{log.creator.name} @ {log.creator.version}</em></div>
-      <div className={cx('table-container')}>
-        <Table className={cx('table')} tbodyRef={tbodyRef}
-          columns={['#', 'Status', 'Method', 'Path', 'Data Size']}
-          onKeyDown={onKeyDown}
-        >
-          {log.entries.map((entry, index) => (
-            <tr key={index} className={cx(index == selectedRow ? ['selected-row', 'render'] : '')}
-              onClick={() => setSelectedRow(index)} >
-              <td>{index + 1}</td>
-              <td>{entry.response.status}</td>
-              <td>{entry.request.method}</td>
-              <td>{trimURL(entry.request.url).trimmedValue}</td>
-              <td>{entry.request.postData == null ? "None" : entry.request.postData.text?.length}</td>
-            </tr>
-          ))}
-        </Table>
-      </div>
-      <div className={cx('body')}>
-        {selectedRow == -1 ? null : <HttpEntry entry={log.entries[selectedRow]} />}
-      </div>
+    <div className={cx('table-container')}>
+      <Table className={cx('table')} tbodyRef={tbodyRef} onKeyDown={onKeyDown}
+        columns={['#', 'Status', 'Method', 'Path', 'Data Size']} >
+        {props.log.entries.map((entry, index) => (
+          <tr key={index} className={cx(index == props.selectedRow ? ['selected-row', 'render'] : '')}
+            onClick={() => props.setSelectedRow(index)} >
+            <td>{index + 1}</td>
+            <td>{entry.response.status}</td>
+            <td>{entry.request.method}</td>
+            <td>{trimURL(entry.request.url).trimmedValue}</td>
+            <td>{entry.request.postData == null ? "None" : entry.request.postData.text?.length}</td>
+          </tr>
+        ))}
+      </Table>
     </div>
   )
 }
 
-const HttpEntry = (props: {
+const HttpDetails = (props: {
   entry: Entry
 }) => (
-  <>
+  <div className={cx('http-details')}>
     <RequestSegment {...props} />
     <ResponseSegment {...props} />
-  </>
+  </div>
 )
 
 const RequestSegment = (props: {
   entry: Entry
-}) => <>
-    <SettingsSection className={cx('section-header')} title="Request" width="full-width">
-      <TabMenu className={cx('tab-group')}
-        tabs={[
-          { id: "request-pretty", label: "Pretty", content: <PrettyHeaders headers={props.entry.request.headers} /> },
-          { id: "request-raw", label: "Raw", content: <RawContent content={requestToRaw(props.entry.request)} /> },
-          { id: "request-content", label: "Content", content: <RequestContent data={props.entry.request.postData} /> },
-        ]}
-      />
-    </SettingsSection>
-  </>
+}) => (
+  <SettingsSection className={cx('section-header')} title="Request" width="full-width">
+    <TabMenu className={cx('tab-group')}
+      tabs={[
+        { id: "request-pretty", label: "Pretty Headers", content: <PrettyHeaders headers={props.entry.request.headers} /> },
+        { id: "request-raw", label: "Raw Headers", content: <RawContent content={requestToRaw(props.entry.request)} /> },
+        { id: "request-content", label: "Post Content", content: <RequestContent data={props.entry.request.postData} /> },
+      ]}
+    />
+  </SettingsSection>
+)
 
 const ResponseSegment = (props: {
   entry: Entry
-}) => <>
-    <SettingsSection className={cx('section-header')} title="Response" width="full-width">
-      <TabMenu className={cx('tab-group')}
-        tabs={[
-          { id: "request-pretty", label: "Pretty Headers", content: <PrettyHeaders headers={props.entry.response.headers} /> },
-          { id: "response-raw", label: "Raw Headers", content: <RawContent content={responseToRaw(props.entry.response)} /> },
-          { id: "response-content", label: "Content", content: <ResponseContent response={props.entry.response} /> },
-        ]}
-      />
-    </SettingsSection>
-  </>
+}) => (
+  <SettingsSection className={cx('section-header')} title="Response" width="full-width">
+    <TabMenu className={cx('tab-group')}
+      tabs={[
+        { id: "request-pretty", label: "Pretty Headers", content: <PrettyHeaders headers={props.entry.response.headers} /> },
+        { id: "response-raw", label: "Raw Headers", content: <RawContent content={responseToRaw(props.entry.response)} /> },
+        { id: "response-content", label: "Content", content: <ResponseContent response={props.entry.response} /> },
+      ]}
+    />
+  </SettingsSection>
+)
 
 const ResponseContent = (props: {
   response: Response
 }) => {
-
   const length = props.response.content.size
   const rawText = props.response.content.text || ''
 
@@ -119,7 +139,6 @@ const ResponseContent = (props: {
 const RequestContent = (props: {
   data?: PostData
 }) => {
-
   if (props.data == null) {
     return <RawContent content="No Post Data captured" />
   }
