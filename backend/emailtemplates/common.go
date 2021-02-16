683410490241e75114112dea1396b5038507a075
Joel Smith <joel.smith@originate.com>
2021-02-15 18:11:17 -0800

    add template, builder for emails

--- backend/emailtemplates/common.go
+++ backend/emailtemplates/common.go
@@ -1,13 +1,25 @@
 package emailtemplates
 
 import (
+	"bytes"
 	"text/template"
 
-	"github.com/theparanoids/ashirt-server/backend/models"
-
-	"github.com/theparanoids/ashirt-server/backend/authschemes/recoveryauth"
-	"github.com/theparanoids/ashirt-server/backend/database"
+	recoveryHelpers "github.com/theparanoids/ashirt-server/backend/authschemes/recoveryauth/helpers"
 	"github.com/theparanoids/ashirt-server/backend/config"
+	"github.com/theparanoids/ashirt-server/backend/database"
+	"github.com/theparanoids/ashirt-server/backend/models"
+)
+
+// EmailTemplate is an enum describing each of the possible email types
+type EmailTemplate = string
+
+const (
+	// EmailRecoveryTemplate contains a message indicating that a user can recover their account
+	EmailRecoveryTemplate EmailTemplate = "self-service-recovery-email"
+
+	// EmailRecoveryDeniedTemplate contains a message indicating that a user CANNOT recover their
+	// account because it's disabled
+	EmailRecoveryDeniedTemplate EmailTemplate = "self-service-recovery-denied-email"
 )
 
 type EmailTemplateData struct {
@@ -16,11 +28,17 @@ type EmailTemplateData struct {
 	DB            *database.Connection
 }
 
+type OutgoingEmail struct {
+	Body  string
+	To    string
+	Error error
+}
+
 var templateFuncs = template.New("base").Funcs(template.FuncMap{
 	"AddRecoveryAuth": func(data EmailTemplateData, label string) (string, error) {
 		// create recovery URL
 		appFrontendRoot := config.FrontendIndexURL()
-		recoveryCode, err := recoveryauth.GenerateRecoveryCodeForUser(data.DB, data.UserRecord.ID)
+		recoveryCode, err := recoveryHelpers.GenerateRecoveryCodeForUser(data.DB, data.UserRecord.ID)
 		recoveryURL := appFrontendRoot + "/login/recovery/" + recoveryCode
 
 		return `<a href="` + recoveryURL + `">` + label + `</a>`, err
@@ -32,3 +50,27 @@ var templateFuncs = template.New("base").Funcs(template.FuncMap{
 		return ""
 	},
 })
+
+// BuildEmail constructs an email message from the given template and template data
+func BuildEmail(emailTemplate EmailTemplate, templateData EmailTemplateData) OutgoingEmail {
+	buf := make([]byte, 0)
+	w := bytes.NewBuffer(buf)
+	var err error
+
+	switch emailTemplate {
+	case EmailRecoveryTemplate:
+		err = recoveryEmail.Execute(w, templateData)
+	case EmailRecoveryDeniedTemplate:
+		err = recoveryDeniedDisabledEmail.Execute(w, templateData)
+	}
+
+	rtn := OutgoingEmail{}
+	if err != nil {
+		rtn.Error = err
+	} else {
+		rtn.Body = string(buf)
+		rtn.To = templateData.UserRecord.Email
+	}
+
+	return rtn
+}
--- backend/emailtemplates/recovery.tmpl.go
+++ backend/emailtemplates/recovery.tmpl.go
@@ -16,3 +16,16 @@ Thanks,
 The ASHIRT Team
 `,
 ))
+
+var recoveryDeniedDisabledEmail = template.Must(templateFuncs.New("recoveryDeniedEmail").Parse(
+	`Hi {{ FullName . }},
+
+A request was made to recover your account. Unfortunately, this account has been disabled. Please first contact an administrator to restore functionality.
+
+If you did not make this request, you can ignore this email.
+
+Thanks,
+
+The ASHIRT Team
+`,
+))
