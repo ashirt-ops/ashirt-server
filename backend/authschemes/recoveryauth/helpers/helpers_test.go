5c09bd5b255e9826b2b50c5b2c4279f5ba618e4b
Joel Smith <joel.smith@originate.com>
2021-02-23 16:15:37 -0800

    add unit tests

---
+++ backend/authschemes/recoveryauth/helpers/helpers_test.go
@@ -0,0 +1,39 @@
+package helpers_test
+
+import (
+	"testing"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/stretchr/testify/require"
+	"github.com/theparanoids/ashirt-server/backend/database"
+	"github.com/theparanoids/ashirt-server/backend/database/seeding"
+	recoveryHelpers "github.com/theparanoids/ashirt-server/backend/authschemes/recoveryauth/helpers"
+	"github.com/theparanoids/ashirt-server/backend/helpers"
+	"github.com/theparanoids/ashirt-server/backend/models"
+	recoveryConsts "github.com/theparanoids/ashirt-server/backend/authschemes/recoveryauth/constants"
+)
+
+func TestGenerateRecoveryCodeForUser(t *testing.T) {
+	db := setupDb(t)
+
+	targetUserID := seeding.UserRon.ID
+	code, err := recoveryHelpers.GenerateRecoveryCodeForUser(db, targetUserID)
+	require.NoError(t, err)
+
+	var recoveryAuthEntry models.AuthSchemeData
+	db.Get(&recoveryAuthEntry, sq.Select("*").From("auth_scheme_data").Where(sq.Eq{
+		"auth_scheme": recoveryConsts.Code,
+		"user_id": targetUserID,
+	}))
+	require.Equal(t, code, recoveryAuthEntry.UserKey)
+}
+
+func setupDb(t *testing.T) *database.Connection {
+	db := seeding.InitTestWithOptions(t, seeding.TestOptions{
+		DatabasePath: helpers.StringPtr("../../../migrations"),
+		DatabaseName: helpers.StringPtr("recovery-auth-test-db"),
+	})
+	seeding.ApplySeeding(t, seeding.HarryPotterSeedData, db)
+
+	return db
+}
