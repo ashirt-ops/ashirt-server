465c892649113fc2aaee491c89d888708772261a
Joel Smith <joel.smith@originate.com>
2021-05-12 16:09:52 -0700

    fix gofmt

--- backend/emailtemplates/recovery.tmpl.go
+++ backend/emailtemplates/recovery.tmpl.go
@@ -9,11 +9,11 @@ const recoveryParagraph = "When you click on the link above, " +
 	"If you have permanently lost the ability to log in normally, " +
 	"you may need to unlink, and then re-link your authentication method. " +
 	"Here are the steps to do that:\n" +
-	"  1. Find your normal authentication method under the \"Authentication Methods\" header.\n" + 
-	"  2. Press the delete button next to your normal authentication method.\n"+
-	"  3. You will be prompted to confirm the deletion by entering the name of the authentication " + 
+	"  1. Find your normal authentication method under the \"Authentication Methods\" header.\n" +
+	"  2. Press the delete button next to your normal authentication method.\n" +
+	"  3. You will be prompted to confirm the deletion by entering the name of the authentication " +
 	"method. Do this, then press the \"Delete\" button.\n" +
-	"  4. Once again, find your normal authentication method under the \"Authentication Methods\" " + 
+	"  4. Once again, find your normal authentication method under the \"Authentication Methods\" " +
 	"header.\n" +
 	"  5. Click the \"Link\" button and, depending on the authentication mechanism, choose new values " +
 	"for your authentication."
