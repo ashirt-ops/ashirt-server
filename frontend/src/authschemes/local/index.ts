2c5f542c475493c20efc36c0cef63b464ab28ae6
Joel Smith <Hikash@gmail.com>
2021-08-05 23:39:44 -0700

    load login/linker via type instead of code

--- frontend/src/authschemes/local/index.ts
+++ frontend/src/authschemes/local/index.ts
@@ -1,12 +1,17 @@
 import Linker from './linker'
 import Login from './login'
 import Settings from './settings'
-import {AuthFrontend} from 'src/authschemes'
+import { AuthFrontend } from 'src/authschemes'
+import { SupportedAuthenticationScheme } from 'src/global_types'
 
 const localAuthFrontend: AuthFrontend = {
   Linker: Linker,
   Login: Login,
   Settings: Settings,
+}
+
+export const configure = (_config: SupportedAuthenticationScheme): AuthFrontend => {
+  return localAuthFrontend
 }
 
 export default localAuthFrontend
