fefd058049273089326cc247f87112950bf5fd3c
Joel Smith <joel.smith@originate.com>
2022-01-24 16:41:11 -0800

    add a component that can copy text or blob

--- frontend/src/components/text_copiers/index.tsx
+++ frontend/src/components/text_copiers/index.tsx
@@ -4,7 +4,7 @@
 import * as React from 'react'
 import classnames from 'classnames/bind'
 
-import { default as Button, ButtonStyle} from 'src/components/button'
+import { default as Button, ButtonStyle } from 'src/components/button'
 import Input from 'src/components/input'
 import Tooltip from 'src/components/tooltip'
 
@@ -15,6 +15,8 @@ export const CopyTextButton = (props: ButtonStyle & {
   title?: string,
   textToCopy: string,
   children?: React.ReactNode,
+  disabled?: boolean,
+  className?: string,
 }) => {
   const [tooltipOpen, setTooltipOpen] = React.useState(false)
 
@@ -46,3 +48,74 @@ export const InputWithCopyButton = (props: {
     <CopyTextButton textToCopy={props.value} title="Copy to clipboard" icon={require('./copy.svg')} />
   </div>
 )
+
+export type CopyBlob = {
+  type: 'blob'
+  content: () => Promise<Blob>
+}
+
+export type CopyText = {
+  type: 'text'
+  content: () => Promise<string>
+}
+
+export const CopyButton = (props: ButtonStyle &
+  (CopyBlob | CopyText) &
+{
+  title?: string,
+  children?: React.ReactNode,
+  disabled?: boolean,
+  className?: string,
+}) => {
+  type SuccessType = "normal" | "success" | "failure"
+  const [showIcon, setShowIcon] = React.useState<SuccessType>("normal")
+
+  const onClick = async (e: React.MouseEvent<Element, MouseEvent>) => {
+    let txt = ''
+    let iconType: SuccessType = "success"
+    try {
+      if (props.type === 'text') {
+        txt = await props.content()
+        await navigator.clipboard.writeText(txt)
+      }
+      else { // image
+        // Jan, 2022: This feature is emerging, and poorly supported
+        // Chrome only supports pngs
+        // firefox has support behind a flag, and does not seem to support jpgs
+        const data = await props.content()
+        // @ts-ignore
+        const item = new ClipboardItem({ [data.type]: data })
+        // @ts-ignore
+        await navigator.clipboard.write([item])
+      }
+    } catch (err) {
+      console.error(err)
+      if (props.type === 'text') {
+        prompt('Failed to copy to clipboard. Please manually copy:', txt)
+      }
+      else {
+        prompt('Failed to copy to clipboard.')
+      }
+      iconType = "failure"
+    }
+
+    setShowIcon(iconType)
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    setShowIcon("normal")
+  }
+  const { className, ...renderProps } = props
+  return (
+    <Button
+      className={cx('copy-success-button', props.className)}
+      {...renderProps}
+      icon={showIcon == 'success'
+        ? require('src/res/success.svg')
+        : showIcon == 'failure'
+          ? require('src/res/error.svg')
+          : require('src/res/copy.svg')
+      }
+      onClick={onClick}
+      children={props.children}
+    />
+  )
+}
