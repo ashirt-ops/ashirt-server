925fc8a616da2d3d1e22a19933eea678b1c13bba
Joel Smith <joel.smith@originate.com>
2022-03-10 22:37:38 -0800

    provide an interface to hide connection/transcation types

---
+++ backend/database/connection_proxy.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	sq "github.com/Masterminds/squirrel"
+)
+
+// ConnectionProxy provides an interface into the database, using either an underlying connection,
+// or a transaction. This is compatible with both Connection and Transactable types
+type ConnectionProxy interface {
+	Select(modelSlice interface{}, builder sq.SelectBuilder) error
+	Get(model interface{}, builder sq.SelectBuilder) error
+	// Exec(query string, values ...interface{}) error // not consistent between Transactable and
+	Insert(table string, valueMap map[string]interface{}) (int64, error)
+	BatchInsert(tableName string, count int, mapFn func(int) map[string]interface{}) error
+	Update(builder sq.UpdateBuilder) error
+	Delete(builder sq.DeleteBuilder) error
+}
