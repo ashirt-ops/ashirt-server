256a2926f7d80a642636b82634a8f179d5af7817
Joel Smith <joel.smith@originate.com>
2022-04-11 10:01:01 -0700

    return evidence metadata from evidence

--- backend/services/list_evidence_for_operation.go
+++ backend/services/list_evidence_for_operation.go
@@ -71,6 +71,10 @@ func ListEvidenceForOperation(ctx context.Context, db *database.Connection, i Li
 	if err != nil {
 		return nil, backend.WrapError("Cannot get tags for evidence", backend.DatabaseErr(err))
 	}
+	metadata, err := metadataForEvidenceByID(db, evidenceIDs)
+	if err != nil {
+		return nil, backend.WrapError("Cannot get metadata for evidence", backend.DatabaseErr(err))
+	}
 
 	evidenceDTO := make([]*dtos.Evidence, len(evidence))
 	for idx, evi := range evidence {
@@ -78,6 +82,10 @@ func ListEvidenceForOperation(ctx context.Context, db *database.Connection, i Li
 
 		if !ok {
 			tags = []dtos.Tag{}
+		}
+		evidenceMetadata, ok := metadata[evi.ID]
+		if !ok {
+			evidenceMetadata = []dtos.EvidenceMetadata{}
 		}
 
 		evidenceDTO[idx] = &dtos.Evidence{
@@ -87,6 +95,7 @@ func ListEvidenceForOperation(ctx context.Context, db *database.Connection, i Li
 			OccurredAt:  evi.OccurredAt,
 			ContentType: evi.ContentType,
 			Tags:        tags,
+			Metadata:    evidenceMetadata,
 		}
 	}
 	return evidenceDTO, nil
