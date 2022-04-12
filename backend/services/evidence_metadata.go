06828852b885a2c590d524fa540dc68334292164
Joel Smith <joel.smith@originate.com>
2022-04-12 13:12:00 -0700

    add ability to update evidence metadata

--- backend/services/evidence_metadata.go
+++ backend/services/evidence_metadata.go
@@ -5,22 +5,24 @@ package services
 
 import (
 	"context"
+	"strings"
 
 	"github.com/theparanoids/ashirt-server/backend"
 	"github.com/theparanoids/ashirt-server/backend/database"
 	"github.com/theparanoids/ashirt-server/backend/policy"
 	"github.com/theparanoids/ashirt-server/backend/server/middleware"
-	// sq "github.com/Masterminds/squirrel"
+
+	sq "github.com/Masterminds/squirrel"
 )
 
-type CreateEvidenceMetadataInput struct {
+type EditEvidenceMetadataInput struct {
 	OperationSlug string
 	EvidenceUUID  string
 	Source        string
 	Body          string
 }
 
-func CreateEvidenceMetadata(ctx context.Context, db *database.Connection, i CreateEvidenceMetadataInput) error {
+func CreateEvidenceMetadata(ctx context.Context, db *database.Connection, i EditEvidenceMetadataInput) error {
 	operation, evidence, err := lookupOperationEvidence(db, i.OperationSlug, i.EvidenceUUID)
 	if err != nil {
 		return backend.WrapError("Unable to create evidence metadata", backend.UnauthorizedWriteErr(err))
@@ -36,7 +38,37 @@ func CreateEvidenceMetadata(ctx context.Context, db *database.Connection, i Crea
 	})
 
 	if err != nil {
+		if strings.Contains(err.Error(), "Error 1062") {
+			return backend.WrapError("Couold not edit evidence metadata",
+				backend.SuggestiveDatabaseErr("This metadata source already exists", err),
+			)
+		}
+
 		return backend.WrapError("Could not create evidence metadata", backend.DatabaseErr(err))
+	}
+
+	return nil
+}
+
+func UpdateEvidenceMetadata(ctx context.Context, db *database.Connection, i EditEvidenceMetadataInput) error {
+	operation, evidence, err := lookupOperationEvidence(db, i.OperationSlug, i.EvidenceUUID)
+	if err != nil {
+		return backend.WrapError("Unable to edit evidence metadata", backend.UnauthorizedWriteErr(err))
+	}
+	if err := policy.Require(middleware.Policy(ctx), policy.CanModifyEvidenceOfOperation{OperationID: operation.ID}); err != nil {
+		return backend.WrapError("Unwilling to edit evidence metadata", backend.UnauthorizedWriteErr(err))
+	}
+
+	err = db.Update(sq.
+		Update("evidence_metadata").
+		Set("body", i.Body).
+		Where(sq.Eq{
+			"evidence_id": evidence.ID,
+			"source":      i.Source,
+		}))
+
+	if err != nil {
+		return backend.WrapError("Could not edit evidence metadata", backend.DatabaseErr(err))
 	}
 
 	return nil
