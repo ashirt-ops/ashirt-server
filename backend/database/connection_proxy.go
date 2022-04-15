56dedc2fef10f8a3ab4d5b8266ef913531ded78a
Joel Smith <joel.smith@originate.com>
2022-04-15 16:53:38 -0700

    add connection proxy (#529)

---
+++ backend/database/connection_proxy.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"context"
+
+	sq "github.com/Masterminds/squirrel"
+)
+
+// ConnectionProxy provides an interface into the database, using either an underlying connection,
+// or a transaction. This is compatible with both Connection and Transactable types
+type ConnectionProxy interface {
+	Select(modelSlice interface{}, builder sq.SelectBuilder) error
+	Get(model interface{}, builder sq.SelectBuilder) error
+	// Exec(query string, values ...interface{}) error // not consistent between Transactable and Connection
+	Insert(table string, valueMap map[string]interface{}) (int64, error)
+	BatchInsert(tableName string, count int, mapFn func(int) map[string]interface{}, onDuplicates ...interface{}) error
+	Update(builder sq.UpdateBuilder) error
+	Delete(builder sq.DeleteBuilder) error
+	WithTx(ctx context.Context, fn func(tx *Transactable)) error
+}
+
+// _verifyConnectionProxyInterface is a "canary" function that ensures that the expected concrete
+// types to the ConnectionProxy interface properly implement the interface.
+//lint:ignore U1000 This is just to verify the interface -- it should never be called directly anyway
+func _verifyConnectionProxyInterface() {
+	var conn *Connection
+	var tx *Transactable
+	check := func(c ConnectionProxy) {}
+	check(conn)
+	check(tx)
+}
