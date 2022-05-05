42e445b0f64aab15e1ae922c3ad19f852db573cc
Joel Smith <joelsmith.developer@gmail.com>
2022-05-05 16:56:59 -0700

    provide mouse event on tag list click

--- frontend/src/components/tag_list/index.tsx
+++ frontend/src/components/tag_list/index.tsx
@@ -4,19 +4,19 @@
 import * as React from 'react'
 import Tag from 'src/components/tag'
 import classnames from 'classnames/bind'
-import {Tag as TagType} from 'src/global_types'
+import { Tag as TagType } from 'src/global_types'
 const cx = classnames.bind(require('./stylesheet'))
 
 export default (props: {
   tags: Array<TagType>,
-  onTagClick?: (t: TagType) => void,
+  onTagClick?: (t: TagType, e: React.MouseEvent) => void,
 }) => (
   <div className={cx('root')}>
     {props.tags.map(tag => (
       <Tag
         key={tag.id}
         name={tag.name}
-        onClick={props.onTagClick ? props.onTagClick.bind(null, tag) : null}
+        onClick={e => props.onTagClick?.(tag, e) ?? null}
         color={tag.colorName}
       />
     ))}
