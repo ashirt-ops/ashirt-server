1d39c8073cf099adb68c9796bccd877fc9f7e4ff
Joel Smith <joel.smith@originate.com>
2022-05-11 20:54:04 -0700

    add helpers

--- backend/helpers/ptr.go
+++ backend/helpers/ptr.go
@@ -1,11 +1,27 @@
+// Copyright 2022, Yahoo Inc.
+// Licensed under the terms of the MIT. See LICENSE file in project root for terms.
+
 package helpers
+
+import "fmt"
 
 // StringPtr converts a string into a *string
 func StringPtr(s string) *string {
 	return &s
 }
 
+// SprintfPtr is a wrapper around Sprintf that returns the result as a string pointer
+func SprintfPtr(s string, vals ...any) *string {
+	whatever := fmt.Sprintf(s, vals...)
+	return StringPtr(whatever)
+}
+
 // I64Ptr converts a int64 into an *int64
 func I64Ptr(i int64) *int64 {
 	return &i
+}
+
+// BoolPtr converts a bool into an *bool
+func BoolPtr(b bool) *bool {
+	return &b
 }
