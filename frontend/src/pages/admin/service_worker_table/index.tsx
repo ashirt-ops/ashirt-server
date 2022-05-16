35c5d53402cf06eb02dd4b92e1ccf2921a6a21d6
Joel Smith <joel.smith@originate.com>
2022-05-16 14:39:26 -0700

    provide admin area to interact with service workers

---
+++ frontend/src/pages/admin/service_worker_table/index.tsx
@@ -0,0 +1,189 @@
+// Copyright 2022, Yahoo Inc.
+// Licensed under the terms of the MIT. See LICENSE file in project root for terms.
+
+import * as React from 'react'
+
+import { ServiceWorker } from 'src/global_types'
+import {
+  renderModals,
+  useModal,
+  useWiredData,
+} from 'src/helpers'
+import { listServiceWorkers, testServiceWorker } from 'src/services'
+
+import SettingsSection from 'src/components/settings_section'
+import {
+  default as Table,
+  ErrorRow,
+  LoadingRow,
+} from 'src/components/table'
+import { default as Button, ButtonGroup } from 'src/components/button'
+import {
+  AddEditServiceWorkerModal,
+  DeleteServiceModal,
+} from './modals'
+
+type WorkerModal = { worker: ServiceWorker }
+
+export default (props: {
+  onReload: (listener: () => void) => void
+  offReload: (listener: () => void) => void
+}) => {
+  const columns: Array<string> = cellOrder()
+
+  const wiredServiceWorkers = useWiredData<Array<ServiceWorker>>(
+    listServiceWorkers,
+    (err) => <ErrorRow span={columns.length} error={err} />,
+    () => <LoadingRow span={columns.length} />
+  )
+  const deleteModal = useModal<WorkerModal>(mProps => (
+    <DeleteServiceModal {...mProps} />
+  ), wiredServiceWorkers.reload)
+  const editModal = useModal<WorkerModal>(mProps => (
+    <AddEditServiceWorkerModal {...mProps} />
+  ), wiredServiceWorkers.reload)
+
+  const [testDataState, dispatchTestData] = React.useReducer(testDataReducer, {})
+
+  React.useEffect(() => {
+    props.onReload(wiredServiceWorkers.reload)
+    return () => { props.offReload(wiredServiceWorkers.reload) }
+  })
+
+  return (
+    <SettingsSection title="Service Worker List" width="wide">
+      <Table columns={columns}>
+        {wiredServiceWorkers.render(data => <>
+          {
+            data.map((worker) => (
+              <tr key={worker.name}>
+                {
+                  cellOrder(worker, testDataState[worker.name] ?? initialTestData, {
+                    showDeleteModal: (worker) => deleteModal.show({ worker }),
+                    showEditModal: (worker) => editModal.show({ worker }),
+                    testService: async (worker) => {
+                      dispatchTestData({ type: 'start', worker: worker.name })
+                      let passedTest = true
+                      try {
+                        await testServiceWorker({ id: worker.id })
+                      }
+                      catch (err) {
+                        passedTest = false
+                      }
+                      dispatchTestData({
+                        type: 'finish',
+                        worker: worker.name,
+                        passedTest,
+                      })
+                    }
+                  }).map((v, colIndex) => (
+                    <td key={worker.name + ":" + columns[colIndex]}>{v}</td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </>)}
+      </Table>
+      {renderModals(deleteModal, editModal)}
+    </SettingsSection>
+  )
+}
+
+type Actions = {
+  showDeleteModal: (worker: ServiceWorker) => void
+  showEditModal: (worker: ServiceWorker) => void
+  testService: (worker: ServiceWorker) => void
+}
+
+const emptyActions: Actions = {
+  showDeleteModal: () => { },
+  showEditModal: () => { },
+  testService: () => { },
+}
+
+function cellOrder(): Array<string>
+function cellOrder(worker: ServiceWorker, testData: TestData, actions: Actions): Array<React.ReactNode>
+function cellOrder(worker?: ServiceWorker, testData?: TestData, actions?: Actions): Array<string | React.ReactNode> {
+  const {
+    showEditModal: showEdit,
+    showDeleteModal: showDelete,
+    testService,
+  } = (actions ?? emptyActions)
+
+  return [
+    worker?.name ?? "Name",
+    (worker?.deleted ?? "Deleted").toString(),
+    testData == undefined ? "Status" : (<WorkerStatusIcon {...testData} />),
+    worker == undefined ? "Actions" : (
+      <>
+        <ButtonGroup>
+          <Button small onClick={() => showEdit(worker)}>Edit</Button>
+          <Button small onClick={() => testService(worker)}>Test</Button>
+          <Button danger small onClick={() => showDelete(worker)}>Delete</Button>
+        </ButtonGroup>
+      </>
+    ),
+  ]
+}
+
+type TestData = {
+  isTesting: boolean,
+  testResult: 'connected' | 'offline' | null
+}
+
+const WorkerStatusIcon = (props: TestData) => {
+  const { isTesting, testResult } = props
+
+  if (isTesting) {
+    return (
+      <div>Testing...</div>
+    )
+  } else if (testResult != null) {
+    return (
+      <div>{testResult === 'connected' ? "Working" : "Offline"}</div>
+    )
+  }
+
+  return null
+}
+
+const initialTestData: TestData = {
+  isTesting: false,
+  testResult: null,
+}
+
+type TestDataState = Record<string, TestData>
+
+const testDataReducer = (state: TestDataState, action: TestDataAction): TestDataState => {
+  if (action.type == 'start') {
+    return {
+      ...state,
+      [action.worker]: { isTesting: true, testResult: null }
+    }
+  }
+  if (action.type == 'finish') {
+    return {
+      ...state,
+      [action.worker]: {
+        isTesting: false,
+        testResult: action.passedTest ? 'connected' : 'offline'
+      }
+    }
+  }
+  return state
+}
+
+type TestDataAction =
+  | TestDataActionStartTest
+  | TestDataActionFinishTest
+
+type TestDataActionStartTest = {
+  type: 'start'
+  worker: string
+}
+type TestDataActionFinishTest = {
+  type: 'finish'
+  passedTest: boolean
+  worker: string
+}
---
+++ frontend/src/pages/admin/service_worker_table/modals.tsx
@@ -0,0 +1,63 @@
+// Copyright 2022, Yahoo Inc.
+// Licensed under the terms of the MIT. See LICENSE file in project root for terms.
+
+import * as React from 'react'
+
+import { ServiceWorker } from 'src/global_types'
+import { useForm, useFormField } from 'src/helpers'
+
+import ChallengeModalForm from 'src/components/challenge_modal_form'
+import { default as Input, TextArea } from 'src/components/input'
+import ModalForm from 'src/components/modal_form'
+import { createServiceWorker, deleteServiceWorkers, updateServiceWorker } from 'src/services'
+
+export const DeleteServiceModal = (props: {
+  worker: ServiceWorker,
+  onRequestClose: () => void,
+}) => (
+  <ChallengeModalForm
+    modalTitle="Delete Service"
+    warningText="This will remove the service from the system. Existing metadata will be kept, but no new metadata will be added by this worker."
+    submitText="Delete"
+    challengeText={props.worker.name}
+    handleSubmit={() => deleteServiceWorkers({ id: props.worker.id })}
+    onRequestClose={props.onRequestClose}
+  />
+)
+
+export const AddEditServiceWorkerModal = (props: {
+  worker?: ServiceWorker,
+  onRequestClose: () => void,
+}) => {
+  const serviceName = useFormField<string>(props.worker?.name ?? "")
+  const serviceConfig = useFormField<string>(props.worker?.config ?? "")
+
+  const handleSubmit = () => {
+    const commonProps = {
+      name: serviceName.value,
+      serviceType: 'aws',
+      config: serviceConfig.value,
+    }
+    return props.worker
+      ? updateServiceWorker({ ...commonProps, id: props.worker.id })
+      : createServiceWorker({ ...commonProps })
+  }
+
+  const formComponentProps = useForm({
+    fields: [serviceName, serviceConfig],
+    handleSubmit,
+    onSuccess: props.onRequestClose
+  })
+  return (
+    <ModalForm
+      title={props.worker ? "Edit Service Worker" : "Create Service Worker"}
+      submitText={props.worker ? "Save" : "Create"}
+      cancelText="Cancel"
+      onRequestClose={props.onRequestClose}
+      {...formComponentProps}
+    >
+      <Input label="Service name" {...serviceName} />
+      <TextArea label="Service Config" {...serviceConfig} />
+    </ModalForm>
+  )
+}
