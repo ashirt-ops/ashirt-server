66bddcb4817800598b1b28cb2a13f84eb302896d
Joel Smith <joel.smith@originate.com>
2022-05-17 12:16:29 -0700

    add demo worker

---
+++ enhancement_worker_templates/demo-tesseract/readme.md
@@ -0,0 +1,7 @@
+# Test Worker
+
+This project implements a basic evidence enhancement service. This is a long running service that can be contacted via an http call (microservice-style).
+
+When executed, the project will perform basic OCR on an image. This _only_ applies to images.
+
+The output will be whatever tesseract spits out
---
+++ enhancement_worker_templates/demo-tesseract/src/services/ashirt.ts
@@ -0,0 +1,137 @@
+import { default as axios, AxiosRequestConfig } from 'axios'
+import { createHmac, createHash } from 'crypto'
+import { formatRFC7231 } from 'date-fns'
+
+export type RequestConfig = {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  path: string
+  body?: string
+  responseType?: 'arraybuffer' | 'document' | 'json' | 'text' | 'stream'
+}
+
+export class AShirtService {
+  private secretKey: Buffer
+  constructor(
+    private apiUrl: string,
+    private accessKey: string,
+    secretKeyB64: string
+  ) {
+    this.secretKey = Buffer.from(secretKeyB64, "base64")
+  }
+
+  async getEvidence(operationSlug: string, evidenceUuid: string) {
+    return this.makeRequest<EvidenceOutput>({
+      method: 'GET',
+      path: `/api/operations/${operationSlug}/evidence/${evidenceUuid}`
+    })
+  }
+
+  async getEvidenceContent(operationSlug: string, evidenceUuid: string, type: 'media' | 'preview' = 'media'): Promise<ResponseWrapper<Buffer>> {
+    return this.makeRequest<Buffer>({
+      method: 'GET',
+      path: `/api/operations/${operationSlug}/evidence/${evidenceUuid}/${type}`,
+      responseType: 'arraybuffer'
+    })
+  }
+
+  async createOperation(body: CreateOperationInput) {
+    return this.makeRequest<Buffer>({
+      method: 'POST',
+      path: `/api/operations`,
+      body: JSON.stringify(body)
+    })
+  }
+
+  async makeRequest<T>(
+    config: RequestConfig,
+    guard?: (o: unknown) => o is T,
+  ): Promise<ResponseWrapper<T>> {
+    const reqConfig = this.buildRequestConfig(config)
+    const resp = await axios(reqConfig)
+    const respData = resp.data
+
+    if (guard && !guard(respData)) {
+      Promise.reject(new Error("Response is not in the right format."))
+    }
+
+    const reqResult: ResponseWrapper<T> = {
+      contentType: resp.headers['content-type'],
+      responseCode: resp.status,
+      data: respData
+    }
+
+    return reqResult
+  }
+
+  private buildRequestConfig(config: RequestConfig) {
+    const sendBody = config.body ?? ''
+    const now = AShirtService.nowInRFC1123()
+    const auth = this.generateAuthorizationHeaderValue({
+      method: config.method,
+      body: sendBody,
+      date: now,
+      path: config.path,
+    })
+
+    const req: AxiosRequestConfig = {
+      method: config.method,
+      url: `${this.apiUrl}${config.path}`,
+      headers: {
+        "Content-Type": "application/json",
+        "Date": now,
+        "Authorization": auth,
+      }
+    }
+    if (sendBody != '') {
+      req.data = sendBody
+    }
+    if (config.responseType) {
+      req.responseType = config.responseType
+    }
+    return req
+  }
+
+  private static nowInRFC1123() {
+    return formatRFC7231(new Date())
+  }
+
+  generateAuthorizationHeaderValue(data: {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE' // more methods with a similar naming style are possible
+    path: string
+    date: string // in RFC1123 format
+    body: string
+  }) {
+    const stringBuff = Buffer.from(
+      data.method + "\n" +
+      data.path + "\n" +
+      data.date + "\n"
+    )
+    // note that this isn't encoded -- the result is a series of raw bytes.
+    const bodyDigest = createHash('sha256').update(data.body).digest()
+
+    const message = Buffer.concat([stringBuff, bodyDigest])
+    const hmacMessage = createHmac('sha256', this.secretKey)
+      .update(message)
+      .digest('base64')
+
+    return `${this.accessKey}:${hmacMessage}`
+  }
+}
+
+type EvidenceOutput = {
+  uuid: string
+  description: string
+  contentType: string
+  occurredAt: Date
+}
+
+type ResponseWrapper<T> = {
+  responseCode: number,
+  contentType: string
+  data: T
+}
+
+type CreateOperationInput = {
+  slug: string
+  name: string
+}
