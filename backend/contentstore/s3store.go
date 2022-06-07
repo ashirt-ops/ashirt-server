82d01401a63a7912a2baa6493b889df680af367f
Joel Smith <joelsmith.developer@gmail.com>
2022-06-07 13:13:01 -0700

    switch to aws-v2 / s3 library

--- backend/contentstore/s3store.go
+++ backend/contentstore/s3store.go
@@ -1,34 +1,37 @@
-// Copyright 2020, Verizon Media
+// Copyright 2022, Yahoo, Inc.
 // Licensed under the terms of the MIT. See LICENSE file in project root for terms.
 
 package contentstore
 
 import (
+	"context"
 	"io"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/google/uuid"
 	"github.com/theparanoids/ashirt-server/backend"
 )
+
+var contentStoreContext = context.Background()
 
 // S3Store is the backing structure needed to interact with an Amazon S3 storage service
 // TODO: this can be unexported
 type S3Store struct {
 	bucketName string
-	s3Client   *s3.S3
+	s3Client   *s3.Client
 }
 
 // NewS3Store provides a mechanism to initialize an S3 bucket in a particular region
 func NewS3Store(bucketName string, region string) (*S3Store, error) {
-	sess, err := session.NewSession()
+	cfg, err := config.LoadDefaultConfig(contentStoreContext)
 	if err != nil {
 		return nil, backend.WrapError("Unable to establish an s3 session", err)
 	}
 	return &S3Store{
 		bucketName: bucketName,
-		s3Client:   s3.New(sess, &aws.Config{Region: &region}),
+		s3Client:   s3.NewFromConfig(cfg),
 	}, nil
 }
 
@@ -44,9 +47,9 @@ func (s *S3Store) Upload(data io.Reader) (string, error) {
 // UploadWithName is a test/dev helper that places a file on S3 with a given name
 // This is not intended for general use.
 func (s *S3Store) UploadWithName(key string, data io.Reader) error {
-	_, err := s.s3Client.PutObject(&s3.PutObjectInput{
-		ACL:    aws.String("bucket-owner-full-control"),
-		Body:   aws.ReadSeekCloser(data),
+	_, err := s.s3Client.PutObject(context.Background(), &s3.PutObjectInput{
+		ACL:    "bucket-owner-full-control",
+		Body:   data,
 		Bucket: aws.String(s.bucketName),
 		Key:    aws.String(key),
 	})
@@ -60,7 +63,7 @@ func (s *S3Store) UploadWithName(key string, data io.Reader) error {
 
 // Read retrieves the indicated file from Amazon S3
 func (s *S3Store) Read(key string) (io.Reader, error) {
-	res, err := s.s3Client.GetObject(&s3.GetObjectInput{
+	res, err := s.s3Client.GetObject(context.Background(), &s3.GetObjectInput{
 		Bucket: aws.String(s.bucketName),
 		Key:    aws.String(key),
 	})
@@ -72,7 +75,7 @@ func (s *S3Store) Read(key string) (io.Reader, error) {
 
 // Delete removes files in in your OS's temp directory
 func (s *S3Store) Delete(key string) error {
-	_, err := s.s3Client.DeleteObject(&s3.DeleteObjectInput{
+	_, err := s.s3Client.DeleteObject(context.Background(), &s3.DeleteObjectInput{
 		Bucket: aws.String(s.bucketName),
 		Key:    aws.String(key),
 	})
--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.18
 require (
 	cloud.google.com/go/storage v1.22.0
 	github.com/Masterminds/squirrel v1.5.2
-	github.com/aws/aws-sdk-go v1.44.4
+	github.com/aws/aws-sdk-go-v2 v1.16.5
+	github.com/aws/aws-sdk-go-v2/config v1.15.10
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.11
 	github.com/coreos/go-oidc/v3 v3.1.0
 	github.com/go-kit/kit v0.12.0
 	github.com/go-sql-driver/mysql v1.6.0
@@ -32,6 +34,20 @@ require (
 	cloud.google.com/go v0.100.2 // indirect
 	cloud.google.com/go/compute v1.5.0 // indirect
 	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.5 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.6 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.6 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.13 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.7 // indirect
+	github.com/aws/smithy-go v1.11.3 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/boombuler/barcode v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
@@ -41,7 +57,7 @@ require (
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/googleapis/gax-go/v2 v2.2.0 // indirect
 	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
