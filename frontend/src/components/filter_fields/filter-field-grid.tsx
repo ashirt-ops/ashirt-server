e4a5acee73720967f6109be1a960fed245e11363
Joel Smith <joel.smith@originate.com>
2022-06-14 09:09:55 -0700

    remove metadata field on evidence type

--- frontend/src/components/filter_fields/filter-field-grid.tsx
+++ frontend/src/components/filter_fields/filter-field-grid.tsx
@@ -266,7 +266,6 @@ const uuidToBasicEvidence = (uuid: string): Evidence => ({
   occurredAt: new Date(),
   tags: [],
   contentType: 'none',
-  metadata: [],
 })
 
 const ChooseEvidenceModal = (props: {
