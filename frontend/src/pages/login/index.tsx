48bfbb1974a0be6a26ae4342f41e5e77ce0b7083
Joel Smith <joel.smith@originate.com>
2022-09-02 14:11:56 -0700

    wip

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -8,6 +8,7 @@ import { getSupportedAuthentications } from 'src/services/auth'
 import { useAuthFrontendComponent } from 'src/authschemes'
 import { useWiredData } from 'src/helpers'
 import { SupportedAuthenticationScheme } from 'src/global_types'
+import Button from 'src/components/button'
 const cx = classnames.bind(require('./stylesheet'))
 
 // This component renders a list of all enabled authscheme login components
@@ -19,25 +20,58 @@ export default () => {
   const location = useLocation()
   const query = new URLSearchParams(location.search)
   const wiredAuthSchemes = useWiredData(getSupportedAuthentications)
+  const [currentAuth, setCurrentAuth] = React.useState<SupportedAuthenticationScheme | null>(null)
 
-  return wiredAuthSchemes.render(supportedAuthSchemes => (
-    <div className={cx('login')}>
-      {supportedAuthSchemes.map((schemeDetails) => {
-        const { schemeCode, schemeType } = schemeDetails
-        if (renderOnlyScheme != null && schemeCode != renderOnlyScheme) return null
-        return (
-          <AuthSchemeLogin
-            key={schemeCode}
-            authSchemeType={schemeType}
-            authScheme={schemeDetails}
-            query={query}
-          />
-        )
-      })}
-    </div>
-  ))
+  return wiredAuthSchemes.render(supportedAuthSchemes => {
+    return (
+      <div className={cx('login')}>
+        {currentAuth == null
+          ? <AuthButtons schemes={supportedAuthSchemes} onSelected={setCurrentAuth} renderOnlyScheme={renderOnlyScheme} />
+          : (
+            <div>
+              <AuthSchemeLogin
+                key={currentAuth.schemeCode}
+                authSchemeType={currentAuth.schemeType}
+                authScheme={currentAuth}
+                query={query}
+              />
+              <Button
+                icon={require('./back.svg')}
+                onClick={() => setCurrentAuth(null)}
+              >
+                Choose different method
+              </Button>
+            </div>
+          )
+        }
+      </div>
+    )
+  })
 }
 
+const AuthButtons = (props: {
+  schemes: Array<SupportedAuthenticationScheme>
+  onSelected: (scheme: SupportedAuthenticationScheme | null) => void
+  renderOnlyScheme?: string
+}) => {
+  return (
+    <div>
+      {
+        props.schemes.map(schemeDetails => {
+          const { schemeCode, schemeName } = schemeDetails
+          if (props.renderOnlyScheme != null && schemeCode != props.renderOnlyScheme) {
+            return null
+          }
+
+          return (
+            <Button onClick={() => props.onSelected(schemeDetails)} >{schemeName}</Button>
+          )
+        })
+      }
+    </div>
+
+  )
+}
 
 const AuthSchemeLogin = (props: {
   authSchemeType: string,
