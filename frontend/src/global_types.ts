f634db4e53d30b260da36c06d45f486c4504239d
Tyler Noblett <Tyler.Noblett@outlook.com>
2022-10-27 10:49:09 -0400

    remove additional frontend references

--- frontend/src/global_types.ts
+++ frontend/src/global_types.ts
@@ -8,18 +8,6 @@ export type Result<T> =
   | ErrorResult
   | SuccessfulResult<T>
 
-export enum OperationStatus {
-  // Values here are the backend representations of OperationStatus defined in backend/models
-  PLANNING = 0,
-  ACTIVE = 1,
-  COMPLETE = 2,
-}
-export const operationStatusToLabel = {
-  [OperationStatus.PLANNING]: "Planning",
-  [OperationStatus.ACTIVE]: "Active",
-  [OperationStatus.COMPLETE]: "Complete",
-}
-
 export type EvidenceViewHint = 'small' | 'medium' | 'large'
 export type InteractionHint = 'active' | 'inactive'
 
