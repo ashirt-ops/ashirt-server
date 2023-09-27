dddddc183015dfcd7ab184681038757e4afc9c99
Tyler Noblett <Tyler.Noblett@outlook.com>
2023-09-27 11:35:25 -0400

    change text

--- frontend/src/pages/admin/add_global_variable/index.tsx
+++ frontend/src/pages/admin/add_global_variable/index.tsx
@@ -12,7 +12,7 @@ export default (props: {
   const [newGlobalVar, setNewGlobalVar] = React.useState<boolean>(false)
 
   return (
-    <SettingsSection title="New Group Creation">
+    <SettingsSection title="New Global Variable Creation">
       <em>
         Creates a global variable, which can be used by service workers to customize the behavior of the application.
       </em>
