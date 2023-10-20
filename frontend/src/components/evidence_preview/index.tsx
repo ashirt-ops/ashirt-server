9c692d4b04b26c85c08a4c1f4b61262c4401253d
Tyler Noblett <Tyler.Noblett@outlook.com>
2023-10-20 07:14:07 -0400

    see if url exists

--- frontend/src/components/evidence_preview/index.tsx
+++ frontend/src/components/evidence_preview/index.tsx
@@ -94,8 +94,8 @@ const EvidenceImage = (props: EvidenceProps) => {
     // TODO TN rename wiredimageinfor
     console.log("about to see wiredImageinfo")
     return wiredImageInfo.render(url => {
-      console.log("___url", url)
-      console.log("___url JSON", JSON.parse(url))
+      console.log("___url", url, url == null, url == undefined, url == "")
+      // console.log("___url JSON", JSON.parse(url))
     return <img src={url} />
   })
   }
