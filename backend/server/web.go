8b9c027e5e68f8da72a72eecc915ac1d6e005f5d
Tyler Noblett <Tyler.Noblett@outlook.com>
2023-10-24 14:04:59 -0400

    remove logs

--- backend/server/web.go
+++ backend/server/web.go
@@ -520,7 +520,6 @@ func bindWebRoutes(r chi.Router, db *database.Connection, contentStore contentst
 		if err != nil {
 			return nil, backend.WrapError("Unable to read evidence", err)
 		}
-		fmt.Println("evidence.Description", evidence.Description)
 		if s3Store, ok := contentStore.(*contentstore.S3Store); ok && evidence.ContentType == "image" {
 			url, err := services.SendUrl(r.Context(), db, s3Store, i)
 			if err != nil {
