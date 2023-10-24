5eb29ac60c2201cabb9143984640fec606ba0fa2
Tyler Noblett <Tyler.Noblett@outlook.com>
2023-10-24 12:50:50 -0400

    correct boolean

--- frontend/src/components/evidence_preview/index.tsx
+++ frontend/src/components/evidence_preview/index.tsx
@@ -79,7 +79,7 @@ const EvidenceCodeblock = (props: EvidenceProps) => {
 }
 
 const EvidenceImage = (props: EvidenceProps) => {
-  if (!props.useS3Url) {
+  if (props.useS3Url) {
     const wiredUrl = useWiredData<string>(React.useCallback(() => getEvidenceAsString({
       operationSlug: props.operationSlug,
       evidenceUuid: props.evidenceUuid,
